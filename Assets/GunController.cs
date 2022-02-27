b391bf86de3c1e160091c8c9e5b1579741931462
sam <samuelssalfi@gmail.com>
2022-02-27 15:26:14 -0400

    Basic sprites, character controller, gun controller

---
+++ Assets/GunController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject player;
+    SpriteRenderer swordRenderer;
+    public GameObject sword;
+    public float angleOffset = 177.3f;
+    public float outwardOffset = -0.085f;
+    public Vector2 positionOffset = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordRenderer = sword.GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate sword
+        sword.transform.localPosition = positionOffset;// new Vector3(0, 0, 0);
+        Vector3 pos = Camera.main.WorldToScreenPoint(sword.transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        if (dir.x < 0)
+        {
+            //renderer.flipX = true;
+            swordRenderer.flipY = false;
+            //angle = angle + 180;
+        }
+        else
+        {
+            //renderer.flipX = false;
+            swordRenderer.flipY = true;
+        }
+        sword.transform.rotation = Quaternion.AngleAxis(angle + angleOffset, Vector3.forward);
+        sword.transform.localPosition += sword.transform.right * outwardOffset;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkspeed = 5;
+
+
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer renderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0) {
+            animator.SetBool("Walking", true);
+        } else
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        rb.velocity = new Vector2(horizontal, vertical).normalized * walkspeed;
+
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        if (dir.x < 0)
+        {
+            renderer.flipX = true;
+        } else
+        {
+            renderer.flipX = false;
+        }
+    }
+}
