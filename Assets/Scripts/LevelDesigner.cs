ee2eb54fc4c9fa524959466367d4003ba892295d
That-Stranger <94190699+That-Stranger@users.noreply.github.com>
2022-03-25 17:53:13 -0300

    Finished Level Design
    Levels are now randomly designed in an interesting way.

--- Assets/Scripts/LevelDesigner.cs
+++ Assets/Scripts/LevelDesigner.cs
@@ -10,8 +10,13 @@ public class LevelDesigner : MonoBehaviour
     public GameObject loot;
     public GameObject oil;
     public GameObject obstacle;
+    public GameObject enemy;
 
     public static int level;
+
+    public int randCheck1;
+    public int randCheck2;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +26,36 @@ public class LevelDesigner : MonoBehaviour
         {
            for (float y = 0; y < 3; y++)
             {
-                float xRand = Random.Range(-3, 4);
-                float yRand = Random.Range(-3, 4);
-                Vector3 position = new Vector3(x * 14 + xRand, y * 14 + 2f + yRand, 0f);
-                CreatePickup(position);
+                int fullness = (int)Random.Range(0, 3);
+
+                for (int i = 0; i < fullness; i++)
+                {
+                    float xRand = Random.Range(-3, 4);
+                    float yRand = Random.Range(-3, 4);
+                    Vector3 position = new Vector3(x * 14 + xRand, y * 14 + 2f + yRand, 0f);
+
+                    CreatePickup(position);
+                }
+                
+                fullness += (int)Random.Range(1, 4);
+
+                for (int i = 0; i < fullness; i++)
+                {
+                    float xRand = Random.Range(-3, 4);
+                    float yRand = Random.Range(-3, 4);
+                    Vector3 position = new Vector3(x * 14 + xRand, y * 14 + 2f + yRand, 0f);
+
+                    CallToFeast(position);
+                }
+
 
                 int numObstacles = Random.Range(0, 3);
 
                 for (; numObstacles > 0; numObstacles--)
                 {
-                    xRand = Random.Range(-2, 3);
-                    yRand = Random.Range(-2, 3);
-                    position = new Vector3(x * 14 + xRand, y * 14 + 2f + yRand, 0f);
+                    float xRand = Random.Range(-2, 3);
+                    float yRand = Random.Range(-2, 3);
+                    Vector3 position = new Vector3(x * 14 + xRand, y * 14 + 2f + yRand, 0f);
                     CreateObstacle(position);
                 }
             }
@@ -64,9 +87,26 @@ public class LevelDesigner : MonoBehaviour
 
         //Obstacles in Doorways
         //Tier 1
-        int where = (int)Random.Range(0, 4);
+        int where = (int)Random.Range(0, 5);
         Tier1Obstacle(where);
-        
+
+        //Tier 2
+        where = (int)Random.Range(0, 5);
+        Tier2Obstacle(where);
+
+        where = (int)Random.Range(where + 2, 7);
+        Tier2Obstacle(where);
+
+        //Tier 3
+        where = (int)Random.Range(2, 6);
+        Tier3Obstacle(where);
+        randCheck1 = where;
+
+        where = (int)Random.Range(0, where - 1);
+        Tier3Obstacle(where);
+        randCheck2 = where;
+
+
     }
 
     // Update is called once per frame
@@ -75,9 +115,10 @@ public class LevelDesigner : MonoBehaviour
         
     }
 
-    //Create a random pickup at the given location
+    //Create a random pickup at the given location. 1/ chance for pickup.
     void CreatePickup(Vector3 position)
     {
+        
         int pickup = Random.Range(0, 100);
 
         if (pickup < 25)
@@ -96,11 +137,17 @@ public class LevelDesigner : MonoBehaviour
         {
             Instantiate(oil, position, Quaternion.identity);
         }
+        
     }
 
     void CreateObstacle(Vector3 position)
     {
         Instantiate(obstacle, position, Quaternion.identity);
+    }
+
+    void CallToFeast(Vector3 position)
+    {
+        Instantiate(enemy, position, Quaternion.identity);
     }
 
     //Takes a number from 0 to 3
@@ -128,10 +175,74 @@ public class LevelDesigner : MonoBehaviour
         CreateObstacle(position);
     }
 
-    //Takes a number from 0 to 7
+    //Takes a number from 0 to 6
     void Tier2Obstacle(int w)
     {
+        float x;
+        float y;
 
+        if (w > 1 && w < 5)
+        {
+            y = 23;
+        }
+        else if (w == 1 || w == 5)
+        {
+            y = 16;
+        }
+        else
+        {
+            y = 2;
+        }
+
+        if (w > 1 && w < 5)
+        {
+            x = 14f * (w - 3);
+        }
+        else if (w < 2)
+        {
+            x = -21;
+        }
+        else
+        {
+            x = 21;
+        }
+
+        Vector3 position = new Vector3(x, y, 0);
+        CreateObstacle(position);
+
+    }
+
+    //Takes a number from 0 to 5
+    void Tier3Obstacle(int w)
+    {
+        float x;
+        float y;
+
+        if (w == 0 || w == 5)
+        {
+            y = 23;
+        }
+        else
+        {
+            y = 30;
+        }
+
+
+        if (w > 0 && w < 5)
+        {
+            x = 14f * (w - 2.5f);
+        }
+        else if (w == 0)
+        {
+            x = -28;
+        }
+        else
+        {
+            x = 28;
+        }
+
+        Vector3 position = new Vector3(x, y, 0);
+        CreateObstacle(position);
     }
 }
 
